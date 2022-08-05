0a39bee9eb752d845172b790f91489334c51b7c5
Mritunjay <mjaymritunjay@gmail.com>
2022-08-06 02:35:39 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var navstatus = 0;
+
+function openNav()
+{
+    if(!navstatus)
+    {
+       $('.maindiv').css('width', '100%') 
+       $('li').css('display','block')
+       navstatus=1
+
+    }
+    else{
+
+        $('.maindiv').css('width', '0')
+        $zz('li').css('display' , 'none')
+        navstatus=0
+
+    }
+}
